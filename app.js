633b556caa0961dd5d796e2578b5d6f00f68b631
javierpontaza <67834557+javierpontaza@users.noreply.github.com>
2022-07-09 11:41:26 -0600

    modificados los archivos online para probar como los jala el github desktop

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log('hola mundo');
 
+/* agregar aca el codigo del mensaje enviado cuando se invoca la funcion*/
+
+function msg(message){
+    console.log('hola amigos, el mensaje es: ', message);
+}
+
+
+/*invocamos*/
+msg('no vamos a ir a ninguna reunion');
+
+
