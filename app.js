2b4ebc07b7eea5af415d9b9ea2e4795a7d848b2f
javierpontaza <67834557+javierpontaza@users.noreply.github.com>
2022-07-09 11:17:08 -0600

    commit numero 7
    commit importante porque es el numero 7 de todos los hechos aca se agregan 1 archivo y se modifican 3

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 console.log('hola mundo');
 
+/* codigo agregado para el commit numero 7 */
+mensaje('hola a todos este es el commit numero 7');
+
+function mensaje(message) {
+    alert(message);
+    console.log(message);
+}
